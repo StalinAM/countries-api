6de34fde3bbf100e71fca21f6a7475ce48e59f4d
Stalin <sta_vla411@hotmail.com>
2023-03-19 18:54:03 -0500

    add header and theme mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from 'styled-components'
+import Header from './components/Header'
+import useThemeMode from './hooks/useThemeMode'
 import GlobalStyles from './styles/GlobalStyles'
-import { dark } from './styles/Theme'
+import { dark, light } from './styles/Theme'
 function App() {
+  const [theme, toggleTheme] = useThemeMode()
   return (
     <>
       <GlobalStyles />
-      <ThemeProvider theme={dark}>HOLA</ThemeProvider>
+      <ThemeProvider theme={theme === 'dark' ? dark : light}>
+        <Header theme={theme} toggleTheme={toggleTheme} />
+      </ThemeProvider>
     </>
   )
 }
