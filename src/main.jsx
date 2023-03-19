84ac0eee0becd263d74fea2b5dff3343fbe511ed
Stalin <sta_vla411@hotmail.com>
2023-03-19 18:53:47 -0500

    add context for api

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import CountriesApi from './context/CountriesApi'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <CountriesApi>
     <App />
-  </React.StrictMode>
+  </CountriesApi>
 )
