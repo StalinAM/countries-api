1211dd17fc9af11880a13b1bc949f81b85f6c68d
Stalin <sta_vla411@hotmail.com>
2023-03-21 19:41:15 -0500

    change color

--- src/styles/Theme.jsx
+++ src/styles/Theme.jsx
@@ -2,7 +2,7 @@ export const dark = {
   elements: 'hsl(209, 23%, 22%)',
   background: 'hsl(207, 26%, 17%)',
   text: 'hsl(0, 0%, 100%)',
-  input: 'hsl(0, 0%, 52%)'
+  input: 'hsl(0, 0%, 98%)'
 }
 export const light = {
   elements: 'hsl(0, 0%, 100%)',
