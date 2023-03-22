cde9a58db9dda87152919fbdf64c88bda020efd4
Stalin <sta_vla411@hotmail.com>
2023-03-21 19:43:47 -0500

    divide the array of countries into 5

--- src/context/CountriesApi.jsx
+++ src/context/CountriesApi.jsx
@@ -3,13 +3,15 @@ import React, { createContext, useEffect, useState } from 'react'
 export const ApiContext = createContext()
 
 function CountriesApi({ children }) {
-  const [countries, setcountries] = useState()
+  const [countries, setCountries] = useState()
+  const [currentPage, setCurrentPage] = useState(1)
+  const [itemsPerPage, setItemsPerPage] = useState(50)
   const url = 'https://restcountries.com/v3.1/all'
   async function loadData() {
     try {
       const response = await fetch(url)
       const data = await response.json()
-      setcountries(data)
+      setCountries(data)
     } catch (error) {
       console.log(error)
     }
@@ -18,8 +20,24 @@ function CountriesApi({ children }) {
   useEffect(() => {
     loadData()
   }, [])
+
+  const indexOfLastItem = currentPage * itemsPerPage
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage
+  const currentItems = countries?.slice(indexOfFirstItem, indexOfLastItem)
+
+  const totalPages = Math.ceil((countries?.length || 0) / itemsPerPage)
   return (
-    <ApiContext.Provider value={{ countries }}>{children}</ApiContext.Provider>
+    <ApiContext.Provider
+      value={{
+        countries: currentItems,
+        currentPage,
+        totalPages,
+        setCurrentPage,
+        setItemsPerPage
+      }}
+    >
+      {children}
+    </ApiContext.Provider>
   )
 }
 
