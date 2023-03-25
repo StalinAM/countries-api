f2dd26ca5a514566aaab78701a8738e4e06ba53f
Stalin <sta_vla411@hotmail.com>
2023-03-24 21:47:49 -0500

    add react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import CountriesApi from './context/CountriesApi'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <CountriesApi>
-    <App />
-  </CountriesApi>
+  <BrowserRouter>
+    <CountriesApi>
+      <App />
+    </CountriesApi>
+  </BrowserRouter>
 )
