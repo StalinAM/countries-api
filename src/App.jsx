e7bfd127eb895d87458485854d1a08ae67152d43
Stalin <sta_vla411@hotmail.com>
2023-03-24 21:47:37 -0500

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import { Route, Routes } from 'react-router-dom'
 import styled, { ThemeProvider } from 'styled-components'
 import Filter from './components/Filter'
 import Header from './components/Header'
 import Pagination from './components/Pagination'
 import useThemeMode from './hooks/useThemeMode'
+import CountrieDetails from './routes/CountrieDetails'
 import GlobalStyles from './styles/GlobalStyles'
 import { dark, light } from './styles/Theme'
 function App() {
@@ -13,8 +15,18 @@ function App() {
       <ThemeProvider theme={theme === 'dark' ? dark : light}>
         <Header theme={theme} toggleTheme={toggleTheme} />
         <Contaier>
-          <Filter />
-          <Pagination />
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <>
+                  <Filter />
+                  <Pagination />
+                </>
+              }
+            />
+            <Route path='/countrie/:id' element={<CountrieDetails />} />
+          </Routes>
         </Contaier>
       </ThemeProvider>
     </>
@@ -25,5 +37,5 @@ export default App
 
 const Contaier = styled.main`
   background-color: ${(props) => props.theme.background};
-  min-height: 100vh;
+  min-height: calc(100vh - 90px);
 `
