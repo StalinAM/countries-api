a8df385a2cc29b4d39a5fcf19f78484229eb5a02
Stalin <sta_vla411@hotmail.com>
2023-03-26 13:52:53 -0500

    refactor and optimized

--- src/context/CountriesApi.jsx
+++ src/context/CountriesApi.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react'
+import React, { createContext, useMemo, useEffect, useState } from 'react'
 
 export const ApiContext = createContext()
 
@@ -9,34 +9,45 @@ function CountriesApi({ children }) {
   const [region, setRegion] = useState('')
   const [wordFilter, setWordFilter] = useState('')
 
-  const url = `https://restcountries.com/v3.1/${
-    region === '' || region === 'all' ? 'all' : `region/${region}`
-  }`
-  async function loadData() {
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      setCountries(data)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+  const url = useMemo(() => {
+    return `https://restcountries.com/v3.1/${
+      region === '' || region === 'all' ? 'all' : `region/${region}`
+    }`
+  }, [region])
 
   useEffect(() => {
+    const loadData = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setCountries(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
     loadData()
-  }, [region, wordFilter])
+  }, [url])
 
-  const indexOfLastItem = currentPage * itemsPerPage
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const currentItems = wordFilter
-    ? countries
-    : countries?.slice(indexOfFirstItem, indexOfLastItem)
+  const filteredCountries = useMemo(() => {
+    if (!wordFilter) {
+      return countries?.slice(
+        (currentPage - 1) * itemsPerPage,
+        currentPage * itemsPerPage
+      )
+    }
+
+    return countries.filter(
+      (country) =>
+        country.name.common.toLowerCase().includes(wordFilter.toLowerCase()) ||
+        country.capital?.[0]?.toLowerCase().includes(wordFilter.toLowerCase())
+    )
+  }, [countries, currentPage, itemsPerPage, wordFilter])
 
   const totalPages = Math.ceil((countries?.length || 0) / itemsPerPage)
   return (
     <ApiContext.Provider
       value={{
-        countries: currentItems,
+        countries: filteredCountries,
         currentPage,
         totalPages,
         setCurrentPage,
